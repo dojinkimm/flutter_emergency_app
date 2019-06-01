365016438f6a74951b48c61ae23ae0f5b6919de3
Henry <dojinkim119@gmail.com>
2019-06-01 20:43:36 +0900

    added press button

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import './screen/home.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Home());
+  }
+}
---
+++ lib/screen/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import './large_scale_building.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: RaisedButton(
+          onPressed: ()=>Navigator.push(context, MaterialPageRoute(builder: (_)=>LargeScaleBuilding())),
+          child: Text("PRESS"),
+        ),
+      ),
+    );
+  }
+}
